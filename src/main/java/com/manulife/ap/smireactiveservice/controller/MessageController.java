a521cfaf0ee2aa0a339d42598dc8f27eef6d5f27
Brenda Hon <Brenda_H_Yu@manulife.com>
2018-11-07 14:48:39 +0800

    Update api path

--- src/main/java/com/manulife/ap/smireactiveservice/controller/MessageController.java
+++ src/main/java/com/manulife/ap/smireactiveservice/controller/MessageController.java
@@ -20,7 +20,7 @@ import reactor.core.publisher.Mono;
  *
  */
 @RestController
-@RequestMapping("/api/v1/messages")
+@RequestMapping("/api/vi/messages")
 public class MessageController {
 
 	@Autowired
